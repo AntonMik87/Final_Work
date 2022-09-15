11ecff329034d22dd4a188ae1adfa6e67afda656
«Anton» <hranitel-liks@mail.ru>
2022-09-15 23:30:11 +0700

    add gitignore

---
+++ Tasks/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+string[] array = new string[] { "123", "sring", "1", "For", "1234", "%", "//" };
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+Console.Write("Результат: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    string arr = array[i];
+    if (arr.Length <= 3)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+
+}
