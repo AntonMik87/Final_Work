8574a2e7758d4a36cbef4b90d80e1b75f7051098
«Anton» <hranitel-liks@mail.ru>
2022-09-15 23:53:00 +0700

    add final cycle

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -6,8 +6,20 @@
 
 Console.Clear();
 
-String[] array = new string[] {"123", "sring", "1", "For", "1234", "%", "//" };
+String[] array = new string[] { "123", "sring", "1", "For", "1234", "%", "//" };
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+Console.Write("Результат: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    string arr = array[i];
+    if(arr.Length <= 3)
+    {
+        Console.Write($"{array[i]} ");
+    }
 }
